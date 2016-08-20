b3113c8cffeb48a101143a4c5ce9928f058bc195
dwhelan <dwhelan@leanintuit.com>
2016-08-20 16:45:11 -0400

    First crack at adding c# kata solution

---
+++ cs/katas/BowlingGame.cs
@@ -0,0 +1,6 @@
+﻿namespace katas
+{
+    public class BowlingGame
+    {
+    }
+}
---
+++ cs/katas/BowlingTest.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace katas
+{
+    [TestFixture]
+    public class BowlingTest
+    {
+       [Test]
+       [Ignore("until doing the kata")]
+        public void should_do_something()
+       {
+           "the answer".Should().Be("42");
+        }
+    }
+}
