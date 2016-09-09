7d7e103e8f698cb3fa464f05de488c902eea6d78
declan <declan@leanituit.com>
2016-09-09 16:55:13 -0400

    Added js roman numeral calculator.

---
+++ js/romanNumeralCalculator/romanNumeralCalculator.spec.js
@@ -0,0 +1,8 @@
+var calculator = require('./romanNumeralCalculator').calculator;
+var expect = require("expect");
+
+describe('Roman Numeral Calculator', function () {
+  xit('should do something', function () {
+    expect("the answer").toBe("42");
+  });
+});
