ed7f1c0d01243d80e5dd28776812ad07d82c8259
declan <declan@leanituit.com>
2016-09-09 16:45:36 -0400

    Added js bowling kata.

---
+++ js/bowling/bowling.js
@@ -0,0 +1,4 @@
+var bowling = function(number){
+};
+
+module.exports.bowling = bowling;
---
+++ js/bowling/bowling.spec.js
@@ -0,0 +1,8 @@
+var bowling = require('./bowling').bowling;
+var expect = require("expect");
+
+describe('Bowling', function () {
+  xit('should do something', function () {
+    expect("the answer").toBe("42");
+  });
+});
