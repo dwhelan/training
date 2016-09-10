3091cfef1806cfd30972ffca1769dec5c0fd29ff
declan <declan@leanituit.com>
2016-09-09 21:20:19 -0400

    Added gilded rose kata to js.

---
+++ js/gilded_rose/gilded_rose.spec.js
@@ -0,0 +1,30 @@
+require('approvals').mocha();
+
+var gildedRose = require('./gilded_rose');
+
+describe("Gilded Rose", function() {
+  it("items", function() {
+    var items = gildedRose.items;
+
+    items.length = 0;
+    items.push(new gildedRose.Item("+5 Dexterity Vest", 10, 20));
+    items.push(new gildedRose.Item("Aged Brie", 2, 50 ));
+    items.push(new gildedRose.Item("Aged Brie", 0, 50 ));
+    items.push(new gildedRose.Item("Aged Brie", -1, 5 ));
+    items.push(new gildedRose.Item("Backstage passes to a TAFKAL80ETC concert", 5, 20 ));
+    items.push(new gildedRose.Item("Backstage passes to a TAFKAL80ETC concert", 5, 49 ));
+    items.push(new gildedRose.Item("Backstage passes to a TAFKAL80ETC concert", 10, 20 ));
+    items.push(new gildedRose.Item("Backstage passes to a TAFKAL80ETC concert", 10, 49 ));
+    items.push(new gildedRose.Item("Backstage passes to a TAFKAL80ETC concert", 11, 20 ));
+    items.push(new gildedRose.Item("Backstage passes to a TAFKAL80ETC concert", 0, 20 ));
+    items.push(new gildedRose.Item("Sulfuras, Hand of Ragnaros", 0, 1 ));
+    items.push(new gildedRose.Item("Sulfuras, Hand of Ragnaros", -1, 1 ));
+    items.push(new gildedRose.Item("Elixir of the Mongoose", 0, 0 ));
+    items.push(new gildedRose.Item("Elixir of the Mongoose", 0, 7 ));
+
+    gildedRose.update_quality();
+
+    var strings = JSON.stringify(items).replace(/[^ \-:,\w\s]/g, "").replace(/,name/g, '\nname');
+    this.verify(strings);
+  });
+});
