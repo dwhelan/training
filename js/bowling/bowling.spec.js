ee2c2325b9c681f2b2631411050129b28a8b8a71
declan <declan@leanituit.com>
2016-09-12 02:35:38 -0700

    Added pins to initial bowling.roll() function in js.

--- js/bowling/bowling.spec.js
+++ js/bowling/bowling.spec.js
@@ -2,7 +2,8 @@ var Bowling = require('./bowling');
 var expect = require("expect");
 
 describe('Bowling', function () {
-  var bowling = new Bowling();
+  var game = new Bowling();
+
   xit('should do something', function () {
     expect("the answer").toBe("42");
   });
