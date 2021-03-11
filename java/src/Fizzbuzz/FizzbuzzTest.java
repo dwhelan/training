8c7786dc5609de116cd6a0313bd6670d3fff5abb
Declan Whelan <declan.whelan@tulip.com>
2021-03-10 23:24:00 -0500

    use junit5.4

--- java/src/Fizzbuzz/FizzbuzzTest.java
+++ java/src/Fizzbuzz/FizzbuzzTest.java
@@ -1,12 +1,11 @@
 package Fizzbuzz;
 
-import org.junit.*;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FizzbuzzTest {
     @Test
-    @Ignore
     public void should_assert_something() {
         assertEquals("1", Fizzbuzz.fizzbuzz(1));
     }
