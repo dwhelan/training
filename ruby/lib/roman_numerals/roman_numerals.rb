d9f5c3bc3561b736a04deeb82ce449cecd019537
George Clark <george@watzan.com>
2021-04-14 16:24:07 -0300

    we tried our hardest

--- ruby/lib/roman_numerals/roman_numerals.rb
+++ ruby/lib/roman_numerals/roman_numerals.rb
@@ -1,4 +1,29 @@
 module RomanNumerals
   def calculate(input)
+    number_of_M = (input / 1000).to_i
+    number_of_D = (input / 500).to_i - (number_of_M * 2)
+    number_of_C = (input / 100).to_i - (number_of_D * 5)
+    number_of_L = (input / 50).to_i - (number_of_C * 2)
+    number_of_X = (input / 10).to_i - (number_of_L * 5)
+    number_of_V = (input / 5).to_i - (number_of_X * 2)
+    number_of_I = input % 5
+    if input < 4
+      'I' * number_of_I
+    elsif input == 4
+      'IV'
+    elsif input >= 5 && input < 9
+      'V' * number_of_V + 'I' * number_of_I
+    elsif input == 9
+      'IX'
+    elsif input >= 10 && input <= 13
+      'X' * number_of_X + 'I' * number_of_I
+    elsif input == 14
+      'XIV'
+    elsif input == 50
+      'L' * number_of_L + 'X' * number_of_X + 'I' * number_of_I
+    elsif input >= 1000
+      'M' * number_of_M
+      
+    end
   end
 end
--- ruby/lib/roman_numerals/roman_numerals_test.rb
+++ ruby/lib/roman_numerals/roman_numerals_test.rb
@@ -5,7 +5,60 @@ require_relative "roman_numerals"
 class RomanNumeralsTest < Minitest::Test
   include RomanNumerals
 
-  def test_start_here
-    assert_equal true, true
+  def test_calculate_below_four
+    assert_equal 'I', calculate(1)
+    assert_equal 'II', calculate(2)
+    assert_equal 'III', calculate(3)
   end
+
+  def test_calculate_four
+    assert_equal 'IV', calculate(4)
+  end
+
+  def test_calculate_five
+    assert_equal 'V', calculate(5)
+  end
+
+  def test_calculate_six_to_eight
+    assert_equal 'VI', calculate(6)
+    assert_equal 'VII', calculate(7)
+    assert_equal 'VIII', calculate(8)
+  end
+
+  def test_calculate_nine
+    assert_equal 'IX', calculate(9)
+  end
+
+  def test_calculate_ten
+    assert_equal 'X', calculate(10)
+  end
+
+  def test_calculate_eleven_to_thirteen
+    assert_equal 'XI', calculate(11)
+    assert_equal 'XII', calculate(12)
+    assert_equal 'XIII', calculate(13)
+  end
+
+  def test_calculate_fourteen
+    assert_equal 'XIV', calculate(14)
+  end
+
+  def test_calculate_fifty
+    assert_equal 'L', calculate(50)
+  end
+
+
+
+  def test_calculate_thousands
+    assert_equal 'M', calculate(1000)
+    assert_equal 'MM', calculate(2000)
+    assert_equal 'MMM', calculate(3000)
+  end
+
+  def test_calculate_2600
+    assert_equal 'something', calculate(2600)
+  end
+
+  
+
 end
